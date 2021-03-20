5bbf69922bdc62562c58cd41932e41ee9c76c3a9
phdafoe <phdafoe@gmail.com>
2021-03-20 17:28:54 +0100

    [CONFIG] - Clean version -

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "CanelakeyChain",
-    platforms: [
-        .iOS(.v9)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
